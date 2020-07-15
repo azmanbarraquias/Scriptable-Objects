08d3ce9b9b7c7a5c546553c1b32699963564e46b
Azman Barraquias <azmanbarraquias@gmail.com>
2020-07-15 19:07:40 +0800

    Commit project to repository

---
+++ Assets/Scriptable Object/Scripts/Card.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://docs.unity3d.com/Manual/class-ScriptableObject.html
+
+/* A ScriptableObject is a data container that you can use to save large amounts of data, 
+ * independent of class instances. One of the main use cases for ScriptableObjects is 
+ * to reduce your Project’s memory usage by avoiding copies of values. 
+ * This is useful if your Project has a Prefab 
+ * that stores unchanging data in attached MonoBehaviour scripts */
+
+// [CreateAssetMenu(fileName ="New Card", menuName = "Card/Minions")] // Submenu
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+    //public string name;
+    public new string name; // new name to differentiate
+
+    [TextArea(3, 10)]
+    public string description;
+
+    public Sprite artwork;
+
+    public int manaCost;
+    public int attack;
+    public int health;
+}
---
+++ Assets/Scriptable Object/Scripts/CardDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card card;
+
+    public TextMeshProUGUI nameTMP;
+    public TextMeshProUGUI descriptionTMP;
+    public Image artwork;
+    public TextMeshProUGUI manaTMP;
+    public TextMeshProUGUI attackTMP;
+    public TextMeshProUGUI healthTMP;
+
+    private void Start()
+    {
+        this.name = card.name;
+        nameTMP.text = card.name;
+        descriptionTMP.text = card.description;
+        artwork.sprite = card.artwork;
+        manaTMP.text = card.manaCost.ToString();
+        attackTMP.text = card.attack.ToString();
+        healthTMP.text = card.health.ToString();
+    }
+}
